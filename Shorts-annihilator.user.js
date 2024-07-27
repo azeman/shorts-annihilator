73f426ebc32d26c272122d7facd7b373665ee991
Ayazel <azeman@users.noreply.github.com>
2024-07-27 22:39:10 +0200

    Removed french

--- Shorts-annihilator.user.js
+++ Shorts-annihilator.user.js
@@ -1,10 +1,8 @@
 // ==UserScript==
 // @name         shorts-annihilator
-// @name:fr      shorts-annihilator
 // @namespace    https://github.com/azeman/shorts-annihilator/
 // @version      1
 // @description  Better, less-toxic Youtube Shorts
-// @description:fr Pour des Youtube Shorts moins cancérigènes
 // @match        *://www.youtube.com/shorts/*
 // @grant        none
 // @license      GNU 3
