14197f1da3be055e5c74ab5a64f23ef9e1aa3364
Ayazel <azeman@users.noreply.github.com>
2024-07-27 22:37:03 +0200

    Added french description

--- Shorts-annihilator.user.js
+++ Shorts-annihilator.user.js
@@ -4,6 +4,7 @@
 // @namespace    https://github.com/azeman/shorts-annihilator/
 // @version      1
 // @description  Better, less-toxic Youtube Shorts
+// @description:fr Pour des Youtube Shorts moins cancérigènes
 // @match        *://www.youtube.com/shorts/*
 // @grant        none
 // @license      GNU 3
